16e1981005422fc81903467ad4663d86f6003a96
Nilson Durval <durval.nilson@gmail.com>
2025-12-18 18:58:28 -0300

    BugFix CourseValidator

--- src/examples/CleanArchAcceleratorTools.Examples/3 - Domain/Aggregates/Courses/Validators/CourseValidator.cs
+++ src/examples/CleanArchAcceleratorTools.Examples/3 - Domain/Aggregates/Courses/Validators/CourseValidator.cs
@@ -13,13 +13,16 @@ public class CourseValidator : AbstractValidator<Course>
 
     private void InstructorId()
     {
-        RuleFor(x => x.InstructorId)
-            .NotEmpty()
-            .WithMessage(x =>
-            {
-                var property = $"{nameof(Course)}.{nameof(Course.InstructorId)}";
-                return $"The property \"{property}\" cannot be empty";
-            });
+        When(x => x.Instructor is null, () =>
+        {
+            RuleFor(x => x.InstructorId)
+                .NotEmpty()
+                .WithMessage(x =>
+                {
+                    var property = $"{nameof(Course)}.{nameof(Course.InstructorId)}";
+                    return $"The property \"{property}\" cannot be empty";
+                });
+        });
     }
 
     private void Title()
