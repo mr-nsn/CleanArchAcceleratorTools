8fe28995884f827ecadd0b8926b1ee95e7569ab3
Nilson Durval <durval.nilson@gmail.com>
2025-12-18 18:57:14 -0300

    Enum extensions to get enum description

---
+++ src/core/CleanArchAcceleratorTools.Domain/Util/EnumExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+using System.Reflection;
+
+namespace CleanArchAcceleratorTools.Domain.Util
+{
+    public static class EnumExtensions
+    {
+        public static string? GetDescription(this Enum value)
+        {
+            Type type = value.GetType();
+            string name = Enum.GetName(type, value) ?? string.Empty;
+            
+            if (!string.IsNullOrEmpty(name))
+            {
+                FieldInfo? field = type.GetField(name);
+                if (field != null)
+                {
+                    DescriptionAttribute? attr = Attribute.GetCustomAttribute(field, typeof(DescriptionAttribute)) as DescriptionAttribute;
+                    if (attr != null)
+                    {
+                        return attr.Description;
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
